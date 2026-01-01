9087a70ab667f75bcd6dc3c5f2259deb5d89a6f0
justatrade <justatrade@mail.ru>
2026-01-01 19:35:23 +0300

    Field type changing for payment system

--- migrations/006_change_payment_system_field_type.sql
+++ migrations/006_change_payment_system_field_type.sql
@@ -1,7 +1,11 @@
 DO $$
 BEGIN
+    ALTER TABLE "payment" ALTER COLUMN payment_system DROP DEFAULT;
+
     ALTER TABLE "payment"
         ALTER COLUMN payment_system
         TYPE paymentsystem
         USING UPPER(payment_system)::paymentsystem;
+
+    ALTER TABLE "payment" ALTER COLUMN payment_system SET DEFAULT 'ROBOKASSA'::paymentsystem;
 END $$;
